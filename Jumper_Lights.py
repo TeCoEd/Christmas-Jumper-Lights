bc35c1dc7fa058c69a5b61d57178c1f3dc806519
Dan Aldred <TeCoEd@users.noreply.github.com>
2015-12-09 21:45:51 +0000

    Create Jumper_Lights.py
    First version of program

---
+++ Jumper_Lights.py
@@ -0,0 +1,71 @@
+###Crimbo lights test##
+import os
+import time;
+import sys, subprocess, urllib, time, tweepy
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.OUT)
+
+print ("HELLO, HAPPY CHRISTMAS JUMPER ENGAGED")
+GPIO.setup(10, GPIO.IN)
+
+####TWITTER SECTION###
+# == OAuth Authentication ==###############
+#
+# This mode of authentication is the new preferred way
+# of authenticating with Twitter.
+
+# The consumer keys can be found on your application's Details
+# page located at https://dev.twitter.com/apps (under "OAuth settings")
+consumer_key= 'xxxxxxxxxxxxx'
+consumer_secret= 'xxxxxxxxxxxxxxxx'
+
+# The access tokens can be found on your applications's Details
+# page located at https://dev.twitter.com/apps (located 
+# under "Your access token")
+access_token= 'xxxxxxxxxxxxxx'
+access_token_secret= 'xxxxxxxxxxxxxxxx'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+##CODE TO GET TWITTER TO LISTEN FOR KEY WORD###
+class Crimbo_Lights(tweepy.StreamListener):
+    def on_status(self, tweet):
+        
+        tweet_to_check = tweet.text ##gets the tweet
+        print tweet_to_check
+        
+        ###Checks for tweets to @PiTests
+        does_the_tweet_contain_key_word = tweet_to_check.find("@PiTests ON")
+        ###change to use code to find key word###
+        print does_the_tweet_contain_key_word
+
+        if does_the_tweet_contain_key_word == 0:
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(10, GPIO.OUT)
+                pic = '/home/pi/lights.jpg' 
+                user = str(tweet.user.screen_name)              
+                time_sent = time.asctime( time.localtime(time.time()) )
+                message = "You turned on the Crimbo Jumper!", time_sent
+                #print error_tweet
+                final_tweet = "@%s" %(user), message
+                #print type(error)
+                api.update_with_media(pic, final_tweet)
+                ###Turn the lights on
+                GPIO.output(10, GPIO.LOW)
+                time.sleep(5)
+                ###Turn them off
+                GPIO.setup(10, GPIO.IN) 
+                
+          
+        else:
+                print "error"
+                 
+stream = tweepy.Stream(auth, Crimbo_Lights())            
+            
+while True:
+    stream.userstream()    
